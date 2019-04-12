43e629c1b05a5d65938339800475e7b519030eb6
Morgan Owen <mog13@live.com>
2019-04-12 17:01:49 +0100

    added nav bar to go home

--- app/src/common/Nav.js
+++ app/src/common/Nav.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import './Nav.scss';
+import {Redirect} from "react-router-dom";
 
 class Nav extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            goHome:false
+        }
+    }
+
+    handleClick = () => {
+        //cannot use react router as outside root. Could bubble up to app but this achieves the same effect
+        window.location.href = window.location.origin
+            ? window.location.origin + '/'
+            : window.location.protocol + '/' + window.location.host + '/';
+    };
+
     render() {
+
+        if (this.state.goHome === true) {
+            return <Redirect push to={`/`}/>
+        }
+
         return (
-            <div className="Nav">
+            <div className="Nav" onClick={()=>{this.handleClick()}}>
                 <h1 className="title"> <i className="fas fa-users fa-x"/> React Users Dashboard</h1>
             </div>
         );
